a90903405d85d198569e1093817918da29a1ca0d
Pchenrique <pchenrique1998@hotmail.com>
2019-11-11 15:34:22 -0300

    parametros da previsão

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ const KEY = "e1a51d8ef4b2b569a4d850bb3bd271ff";
 
 class App extends React.Component{
    state = {
+     hora: undefined,
      temperatura: undefined,
+     temp_max: undefined,
+     temp_min: undefined,
      cidade: undefined,
      pais: undefined,
      umidade: undefined,
      descricao: undefined,
+     velocidade_vento: undefined,
      error: undefined
    }
   getClima = async (e) =>{
@@ -21,24 +25,45 @@ class App extends React.Component{
     const pais = e.target.elements.pais.value; 
     const chamada_api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade},${pais}uk&appid=${KEY}&units=metric`);
     const data = await chamada_api.json();
-    if(cidade && pais){
-      console.log(data);
+    if(data.cod === "404"){
       this.setState({
-        temperatura: data.main.temp,
-        cidade: data.name,
-        pais: data.sys.country,
-        umidade: data.main.humidity,
-        descricao: data.weather[0].description,
-        error: "" 
-      });
-    }else{
-      this.setState({
+        hora: undefined,
         temperatura: undefined,
+        temp_max: undefined,
+        temp_min: undefined,
         cidade: undefined,
         pais: undefined,
         umidade: undefined,
         descricao: undefined,
-        error: "Por favor entre com cidade e pais." 
+        velocidade_vento: undefined,
+        error: "Por favor entre com cidade e pais validos." 
+      });
+    }else if(cidade && pais){
+      console.log(data);
+      this.setState({
+        hora: data.dt,
+        temperatura: data.main.temp,
+        temp_max: data.main.temp_max,
+        temp_min: data.main.temp_min,
+        cidade: data.name,
+        pais: data.sys.country,
+        umidade: data.main.humidity,
+        descricao: data.weather[0].description,
+        velocidade_vento: data.wind.speed,
+        error: "" 
+      });
+    }else{
+      this.setState({
+        hora: undefined,
+        temperatura: undefined,
+        temp_max: undefined,
+        temp_min: undefined,
+        cidade: undefined,
+        pais: undefined,
+        umidade: undefined,
+        descricao: undefined,
+        velocidade_vento: undefined,
+        error: "Erro" 
       });
     }
   }
@@ -48,11 +73,15 @@ class App extends React.Component{
         <Title />
         <Form getClima={this.getClima}/>      
         <Clima 
+          hora={this.state.hora}
           temperatura={this.state.temperatura}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
           cidade={this.state.cidade}
           pais={this.state.pais}
           umidade={this.state.umidade}
           descricao={this.state.descricao}
+          velocidade_vento={this.state.velocidade_vento}
           error={this.state.error}
         />
       </div>
--- src/components/clima.js
+++ src/components/clima.js
@@ -4,10 +4,14 @@ class Clima extends React.Component{
     render(){
         return(
             <div>
+                {this.props.hora && <p>Horario: {this.props.hora}</p>}
                 {this.props.cidade && this.props.pais && <p>Localização: {this.props.cidade},{this.props.pais}</p>}
                 {this.props.temperatura && <p>Temperatura: {this.props.temperatura}</p>}
+                {this.props.temp_max && <p>Temperatura maxima: {this.props.temp_max}</p>}
+                {this.props.temp_min && <p>Temperatura minima: {this.props.temp_min}</p>}
                 {this.props.umidade && <p>Umidade: {this.props.umidade}%</p>}
                 {this.props.descricao && <p>Descrição: {this.props.descricao}</p>}
+                {this.props.velocidade_vento && <p>Velocidade do vento: {this.props.velocidade_vento}m/s</p>}
                 {this.props.error && <p>{this.props.error}</p>} 
             </div>
         );
